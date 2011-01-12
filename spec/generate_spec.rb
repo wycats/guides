0d0457d417d084b221e4bf3e1eb7bc7496fe17ac
wycats <wycats@gmail.com>
2011-01-11 22:42:49 -0800

    More generation specs

--- spec/generate_spec.rb
+++ spec/generate_spec.rb
@@ -17,6 +17,27 @@ describe "guides generate" do
     out.should =~ /Generating contribute.html.*Generating credits.html.*Generating index.html/m
   end
 
+  it "generates assets" do
+    files = Dir["output/*"]
+
+    File.directory?("output/images").should be_true
+    File.directory?("output/javascripts").should be_true
+    File.directory?("output/stylesheets").should be_true
+    File.file?("output/javascripts/guides.js").should be_true
+    File.file?("output/stylesheets/main.css").should be_true
+    File.file?("output/stylesheets/overrides.style.css").should be_true
+  end
+
+  it "does nothing if run twice in a row" do
+    guides "generate"
+    out.should be_blank
+  end
+
+  it "re-runs if run with --clean" do
+    guides "generate", "--clean"
+    out.should =~ /Generating contribute.html.*Generating credits.html.*Generating index.html/m
+  end
+
   it "creates index.html" do
     File.read("output/index.html").should =~ /<a href="contribute.html">/
   end
